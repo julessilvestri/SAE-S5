55f856135ac266e05910f5daf51ff0241499902f
julessilvestri <jules.sil05@gmail.com>
2023-12-20 08:01:28 +0100

    commit choice

--- main.py
+++ main.py
@@ -2,73 +2,111 @@ import influxdb_client
 from influxdb_client.client.write_api import SYNCHRONOUS
 import csv
 
+# Recommandation de l'ADEME
+def getStatusComfort(value, measurement) :
+    if measurement == "°C" and value < 18 : # température
+        return 'inconfortable'
+    elif measurement == "%" and value < 40 or record.get_value() > 60 : # humidité
+        return 'inconfortable'
+    elif measurement == "ppm" and value > 1000 : # CO2
+        return 'inconfortable'
+    elif measurement == "UV index" and value > 2 : # UV
+        return 'inconfortable'
+    elif measurement == "dBA" and value > 100 : # dBA
+        return 'inconfortable'
+    elif measurement == "µg/m³" and value > 10 : # µg/m³
+        return 'inconfortable'
+    else :  
+        return ''
+
 bucket = "IUT_BUCKET"
 org = "INFO"
 token = "q4jqYhdgRHuhGwldILZ2Ek1WzGPhyctQ3UgvOII-bcjEkxqqrIIacgePte33CEjekqsymMqWlXnO0ndRhLx19g=="
 
 url = "http://51.83.36.122:8086/"
 
-client = influxdb_client.InfluxDBClient(
-    url=url,
-    token=token,
-    org=org
-)
-
-query_api = client.query_api()
-
-
-sensorQuery = 'from(bucket: "' + bucket +'")\
-    |> range(start:-2d)\
-    |> filter(fn: (r) => r["_field"] == "value")\
-    |> distinct(column: "entity_id")\
-    |> group(columns: ["entity_id"], mode:"by")'
-
-sensorResult = query_api.query(org=org, query=sensorQuery)
-
-sensorResults = []
-for table in sensorResult:
-  for record in table.records:
-    sensorResults.append(
-        record.values.get("entity_id")
+try:
+    client = influxdb_client.InfluxDBClient(
+        url=url,
+        token=token,
+        org=org
     )
 
-i = 0
-for s in sensorResults :
-   print(str(i) + " => " + str(s))
-   i += 1
+    query_api = client.query_api()
 
-entity = int(input("Capteur : "))
+    sensors = [
+        "d251_1",
+        "d351_1",
+        "d351_2",
+        "d351_3",
+        "d360_1"
+    ]
 
-sensor = sensorResults[entity]
-print(sensor)
+    for index, sensor in enumerate(sensors):
+        print(str(index) + " => " + str(sensor))
 
-query = 'from(bucket: "' + bucket +'")\
- |> range(start:-2d)\
- |> filter(fn: (r) => r["entity_id"] == "' + sensor + '")\
- |> filter(fn: (r) => r["_field"] == "value")\
- |> filter(fn: (r) => r["_measurement"] == "°C")'
+    print ('')
+    entity = int(input("Capteur : "))
+    print ('')
 
-result = query_api.query(org=org, query=query)
+    measurementQuery = 'from(bucket: "' + bucket +'")\
+        |> range(start:-2d)\
+        |> filter(fn: (r) => r["_field"] == "value")\
+        |> filter(fn: (r) => r["entity_id"] =~ /^' + sensors[entity] + '/)\
+        |> group(columns: ["_measurement"], mode:"by")\
+        |> distinct(column: "_measurement")'
+    
+    try:
+        measurementResult = query_api.query(org=org, query=measurementQuery)
 
-results = []
-for table in result:
-  for record in table.records:
-    state = ""
-    if record.get_value() < 18 :
-       state = 'inconfortable'
-    results.append((
-       record.get_field(), 
-       record.get_value(), 
-       record.get_measurement(),
-       record.get_time(),
-       state
-    ))
+        measurementResults = []
+        for table in measurementResult:
+            for record in table.records:
+                measurementResults.append(
+                    record.values.get("_measurement")
+                )
 
-csv_file_path = 'exported_data.csv'
+        for index, measurement in enumerate(measurementResults):
+            print(str(index) + " => " + str(measurement))
+    except:
+        print("Erreur requête flux")
+        
 
-with open(csv_file_path, mode='w', newline='') as csv_file:
-    csv_writer = csv.writer(csv_file)
-    csv_writer.writerow(['_field', '_value', '_measurement', '_time', '_state'])
-    csv_writer.writerows(results)
+    print ('')
+    measurement = int(input("Mesure : "))
+    print ('')
 
-print(f'Data exported to {csv_file_path}')
+    query = 'from(bucket: "' + bucket +'")\
+    |> range(start:-2d)\
+    |> filter(fn: (r) => r["entity_id"] =~ /^' + sensors[entity] + '/)\
+    |> filter(fn: (r) => r["_field"] == "value")\
+    |> filter(fn: (r) => r["_measurement"] == "' + measurementResults[measurement] +'")'
+
+    try:
+        result = query_api.query(org=org, query=query)
+
+        results = []
+        for table in result:
+            for record in table.records:                
+                results.append((
+                    record.get_field(), 
+                    record.get_value(), 
+                    record.get_measurement(),
+                    record.get_time(),
+                    getStatusComfort(record.get_value(), record.get_measurement())
+                ))
+
+        csv_file_path = 'exported_data.csv'
+
+        with open(csv_file_path, mode='w', newline='') as csv_file:
+            csv_writer = csv.writer(csv_file)
+            csv_writer.writerow(['_field', '_value', '_measurement', '_time', '_state'])
+            csv_writer.writerows(results)
+
+        print(f'Data exported to {csv_file_path}')
+    except:
+        print("Erreur requête flux")
+except:
+    print("Erreur connexion à InfluxDB")
+
+
